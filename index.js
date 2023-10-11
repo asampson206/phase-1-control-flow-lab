333c32957a1b7a1093d684b6707969b8d451e642
alexander sampson <a.sampson206@gmail.com>
2023-10-10 18:37:37 -0700

    wow

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  let freeSample = 'This one is on me!'
+  let firstPrice = 'That will be twenty bucks.'
+  let nextPrice = 'I will gladly take your thirty bucks.'
+  let lastPrice = 'No can do.'
+if (ride <= 400){return freeSample} if (ride <2000){return firstPrice} if(ride <= 2500){return nextPrice} else{return lastPrice}
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+let myCity = 'Ok, sounds good.'
+let noNyc = 'NYC'
+let noCity = "No go."
+return city === noNyc? myCity: noCity
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+
+switch(tip) {
+case "generous" : return 'Thank you so much.'; break;
+case 'not as generous' : return 'Thank you.'; break;
+default: return'Bye.'; break;
+}
+ 
+
 }
